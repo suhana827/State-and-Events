bc3b729cfdb8cbe1e9c8e7f28e040d4959ece2e5
suhana783 <suhanachauhan133@gmail.com>
2025-08-17 15:55:21 +0530

    updated

---
+++ React-folder/react-app/src/App.jsx
@@ -0,0 +1,37 @@
+// 1. Counter 
+// import React from "react";
+// import Counter from "./State_Events.jsx";  
+
+
+// function App() {
+//   return (
+//     <div>
+//       <Counter />
+//     </div>
+//   );
+// }
+
+//  export default App;
+
+
+
+// 2. Form 
+import React from "react";
+import Form from "./Form";   // importing Form component
+
+function App() {
+  return (
+    <div>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
---
+++ React-folder/react-app/src/Form.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import "./Form.css";
+
+function Form() {
+  const [text, setText] = useState("");
+
+  return (
+    <div className="form-container">
+      <h2>Live Text Form</h2>
+      <input
+        type="text"
+        placeholder="Type something..."
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <p>You typed: {text}</p>
+    </div>
+  );
+}
+
+// export default Form;
+export default Form;
